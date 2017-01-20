928590023b7f6c70d4cfca37243cea91629cc368
ryliemn <ryliemn@gmail.com>
2017-01-19 16:29:40 -0800

    tmp max depth

---
+++ src/solutions/maximumdepthofbinarytree/TreeNode.java
@@ -0,0 +1,12 @@
+package solutions.maximumdepthofbinarytree;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        this.val = x;
+    }
+}
